df15740b9edc3616f69c7ed68b7a0b6970d3a101
Solomon Odunusi <solomonodunusi@gmail.com>
2024-05-02 03:00:10 +0100

    Making rest request testing

--- server/server.js
+++ server/server.js
@@ -5,13 +5,25 @@ import 'dotenv/config'
 const app = express();
 let PORT = 3000;
 
+// Middleware to allow json
+app.use(express.json())
+
 // Connect to the mongo database
 mongoose.connect(process.env.DB_LOCATION, {
     autoIndex: true
 })
 
-app.port("/signup", (req, res) => {
-    res.
+// Routes in the server
+app.post("/signup", (req, res) => {
+
+    // Destructure from request body
+    let {fullname, email, password} = req.body
+
+    // Validate the data
+    if (email.length < 3) {
+        return res.status(403).json({"Error": "Email must be at least 3 letters long"})
+    }
+    return res.status(200).json({"Status": "Success"})
 })
 
 app.listen(PORT, () => {
